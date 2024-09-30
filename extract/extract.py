18db2f9933248e8471ee8af0837e373f00d19658
robert112233 <robertanderson112233@gmail.com>
2024-09-30 14:45:31 +0100

    Initial Extract Infrastructure Created

---
+++ extract/extract.py
@@ -0,0 +1,2 @@
+def extract(event, context):
+    return "hello"
---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_extract_lambda" {
+  name               = "iam_for_extract_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "extract_lambda_script" {
+  type        = "zip"
+  source_file = "${path.module}/../extract/extract.py"
+  output_path = "${path.module}/../extract/extract_lambda_payload.zip"
+}
+
+resource "aws_lambda_function" "extract_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "${path.module}/../extract/extract_lambda_payload.zip"
+  function_name = "extract"
+  role          = aws_iam_role.iam_for_extract_lambda.arn
+  handler       = "extract.extract"
+
+  source_code_hash = data.archive_file.extract_lambda_script.output_base64sha256
+
+  runtime = "python3.11"
+
+}
