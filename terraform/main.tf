c1e5d8882b27c748b572c15d532ddc5425f227eb
robert112233 <robertanderson112233@gmail.com>
2024-09-30 19:29:35 +0100

    s3 raw data bucket created and filled

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,14 +18,14 @@ resource "aws_iam_role" "iam_for_extract_lambda" {
 
 data "archive_file" "extract_lambda_script" {
   type        = "zip"
-  source_file = "${path.module}/../extract/extract.py"
-  output_path = "${path.module}/../extract/extract_lambda_payload.zip"
+  source_file = "${path.module}/../ETL/extract/extract.py"
+  output_path = "${path.module}/../ETL/extract/extract_lambda_payload.zip"
 }
 
 resource "aws_lambda_function" "extract_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "${path.module}/../extract/extract_lambda_payload.zip"
+  filename      = "${path.module}/../ETL/extract/extract_lambda_payload.zip"
   function_name = "extract"
   role          = aws_iam_role.iam_for_extract_lambda.arn
   handler       = "extract.extract"
@@ -34,4 +34,14 @@ resource "aws_lambda_function" "extract_lambda" {
 
   runtime = "python3.11"
 
+}
+
+resource "aws_s3_bucket" "raw_data" {
+  bucket = "imdb-etl-raw-data-bucket"
+}
+
+resource "aws_s3_object" "object" {
+  bucket = "imdb-etl-raw-data-bucket"
+  key    = "movies.csv"
+  source = "${path.module}/../data/movies.csv"
 }
